d957b7db191bc81fd34cc5914f3480655e13df87
RickRossUs <ricardosr@estudiantes.uci.cu>
2023-12-30 00:42:33 +0100

    Cambios package.json

--- src/data/DBanner.ts
+++ src/data/DBanner.ts
@@ -1,7 +1,7 @@
 const EnBanner = {
     hello: 'HI THERE!',
     me: "I'M",
-    content: "I'M UI/UX Designer and Developer with over 5+ years of experience and beautiful web applications through carefully crafted design.",
+    content: "I'M UI/UX Designer and Developer with over 2+ years of experience and beautiful web applications through carefully crafted design.",
     btnAboutMe: "MORE ABOUT ME",
     btnPortfolio: "MY PORTFOLIO"
 }
@@ -9,7 +9,7 @@ const EnBanner = {
 const EsBanner = {
     hello: '¡HOLA!',
     me: 'SOY',
-    content: 'Soy diseñador y desarrollador de UI / UX con más de 5+ años de experiencia y hermosas aplicaciones web a través de un diseño cuidadosamente elaborado.',
+    content: 'Soy diseñador y desarrollador de UI / UX con más de 2+ años de experiencia y hermosas aplicaciones web a través de un diseño cuidadosamente elaborado.',
     btnAboutMe: "MÁS SOBRE MÍ",
     btnPortfolio: "MI PORTAFOLIO"
 }
